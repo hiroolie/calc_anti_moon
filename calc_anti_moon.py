950b72d9d8721021bb5bc6f961e2ada61f2673ee
hiroolie <hiroolie@gmail.com>
2024-06-29 05:15:39 +0900

    Update calc_anti_moon.py
    Adjust appearance

--- calc_anti_moon.py
+++ calc_anti_moon.py
@@ -17,65 +17,65 @@ st.set_page_config(layout="wide")
 
 ### Definition of functions
 def get_first_date(itime):
-    return itime.replace(day=1)
+  return itime.replace(day=1)
 
 def get_last_date(itime):
-    return itime.replace(day=calendar.monthrange(itime.year, itime.month)[1])
+  return itime.replace(day=calendar.monthrange(itime.year, itime.month)[1])
 
 def date_range(start_date, end_date):
-    diff = (end_date - start_date).days + 1
-    return (start_date + timedelta(i) for i in range(diff))
+  diff = (end_date - start_date).days + 1
+  return (start_date + timedelta(i) for i in range(diff))
 
 def mkdataframe(fdate, ldate, method):
-    df = pd.DataFrame()
-    row_list = []
+  df = pd.DataFrame()
+  row_list = []
 
-    def calc_mlight(vp):
-        moon.compute(vp)
-        ALTITUDE = deg(moon.alt)
-        PHASE = moon.moon_phase * deg(moon.alt)
-        tALTITUDE = ALTITUDE / 90
-        return round(tALTITUDE * PHASE, 6)
-    def phase_mlight(vp):
-        moon.compute(vp)
-        return round(moon.moon_phase, 6)
-    def alt_mlight(vp):
-        moon.compute(vp)
-        return round(deg(moon.alt), 6)
+  def calc_mlight(vp):
+    moon.compute(vp)
+    ALTITUDE = deg(moon.alt)
+    PHASE = moon.moon_phase * deg(moon.alt)
+    tALTITUDE = ALTITUDE / 90
+    return round(tALTITUDE * PHASE, 6)
+  def phase_mlight(vp):
+    moon.compute(vp)
+    return round(moon.moon_phase, 6)
+  def alt_mlight(vp):
+    moon.compute(vp)
+    return round(deg(moon.alt), 6)
 
-    for DATE in date_range(get_first_date(itime), get_last_date(itime)):
-        lday = DATE.strftime("%Y/%m/%d")
-        list_ = [lday]
-        for TIME in range (12,24):
-            u_DATE = timezone('UTC').localize(DATE)
-            vp.date = dt.datetime(u_DATE.year, u_DATE.month, u_DATE.day, TIME, 30, 0, 0) - dt.timedelta(hours=9)
-            if method == "calc" :
-                list_.append(calc_mlight(vp))
-            elif method == "phase" :
-                list_.append(phase_mlight(vp))
-            elif method == "alt" :
-                list_.append(alt_mlight(vp))
-            else:
-                exit
-        for TIME in range (0,12):
-            u_DATE = timezone('UTC').localize(DATE)
-            p_DATE = u_DATE + timedelta(days=1)
-            vp.date = dt.datetime(p_DATE.year, p_DATE.month, p_DATE.day, TIME, 30, 0, 0) - dt.timedelta(hours=9)
-            if method == "calc" :
-                list_.append(calc_mlight(vp))
-            elif method == "phase" :
-                list_.append(phase_mlight(vp))
-            elif method == "alt" :
-                list_.append(alt_mlight(vp))
-            else:
-                exit
-        row_list.append(list_)
-        list_ = []
-    df = pd.DataFrame(row_list, columns = ['Date','12:30','13:30','14:30','15:30','16:30','17:30','18:30','19:30','20:30','21:30','22:30','23:30','0:30','1:30','2:30','3:30','4:30','5:30','6:30','7:30','8:30','9:30','10:30','11:30'])
-    df["Date"] = pd.to_datetime(df["Date"])
-    df["Date"] = df["Date"].dt.date
-    df = df.set_index("Date")
-    return df
+  for DATE in date_range(get_first_date(itime), get_last_date(itime)):
+    lday = DATE.strftime("%Y/%m/%d")
+    list_ = [lday]
+    for TIME in range (12,24):
+      u_DATE = timezone('UTC').localize(DATE)
+      vp.date = dt.datetime(u_DATE.year, u_DATE.month, u_DATE.day, TIME, 30, 0, 0) - dt.timedelta(hours=9)
+      if method == "calc" :
+        list_.append(calc_mlight(vp))
+      elif method == "phase" :
+        list_.append(phase_mlight(vp))
+      elif method == "alt" :
+        list_.append(alt_mlight(vp))
+      else:
+        exit
+    for TIME in range (0,12):
+      u_DATE = timezone('UTC').localize(DATE)
+      p_DATE = u_DATE + timedelta(days=1)
+      vp.date = dt.datetime(p_DATE.year, p_DATE.month, p_DATE.day, TIME, 30, 0, 0) - dt.timedelta(hours=9)
+      if method == "calc" :
+        list_.append(calc_mlight(vp))
+      elif method == "phase" :
+        list_.append(phase_mlight(vp))
+      elif method == "alt" :
+        list_.append(alt_mlight(vp))
+      else:
+        exit
+    row_list.append(list_)
+    list_ = []
+  df = pd.DataFrame(row_list, columns = ['Date','12:30','13:30','14:30','15:30','16:30','17:30','18:30','19:30','20:30','21:30','22:30','23:30','0:30','1:30','2:30','3:30','4:30','5:30','6:30','7:30','8:30','9:30','10:30','11:30'])
+  df["Date"] = pd.to_datetime(df["Date"])
+  df["Date"] = df["Date"].dt.date
+  df = df.set_index("Date")
+  return df
 
 ### Initializing Objects
 ## Arguments for period specification
@@ -112,29 +112,29 @@ st.subheader('算出する期間を選択し、地図から観測場所を選択
 st.caption('月の輝面率と高さを掛けてみただけなので本当の明るさかどうかは不明。')
 
 with st.container():
-    # with st.expander('観測地点をクリック'):
-      st_data = st_folium(m, width=1280, height=500)
-      if st_data["last_clicked"] is not None:
-        st.write("Lat[緯度]:", st_data["last_clicked"].get('lat', 'not found'), "Lon[経度]:", st_data["last_clicked"].get('lng', 'not found'))
+  with st.expander('観測地点をクリック'):
+    st_data = st_folium(m, width=1280, height=500)
+    if st_data["last_clicked"] is not None:
+    st.write("Lat[緯度]:", st_data["last_clicked"].get('lat', 'not found'), "Lon[経度]:", st_data["last_clicked"].get('lng', 'not found'))
 
 
 with st.container():
-    if st_data["last_clicked"] is not None:
-        i_locate = st_data["last_clicked"]
+  if st_data["last_clicked"] is not None:
+    i_locate = st_data["last_clicked"]
 
-        vp.lat = str(round(i_locate['lat'], 6))
-        vp.lon = str(round(i_locate['lng'], 6))
-        vp.elevation = 500
+    vp.lat = str(round(i_locate['lat'], 6))
+    vp.lon = str(round(i_locate['lng'], 6))
+    vp.elevation = 500
 
-        df_brightness = mkdataframe(get_first_date(itime), get_last_date(itime), "calc")
-        df_phase = mkdataframe(get_first_date(itime), get_last_date(itime), "phase")
-        df_alt = mkdataframe(get_first_date(itime), get_last_date(itime), "alt")
+    df_brightness = mkdataframe(get_first_date(itime), get_last_date(itime), "calc")
+    df_phase = mkdataframe(get_first_date(itime), get_last_date(itime), "phase")
+    df_alt = mkdataframe(get_first_date(itime), get_last_date(itime), "alt")
 
-        with st.expander('夜空の明るさの推測値 @ 0.00:暗 - 99.0:明', expanded=True):
-            st.dataframe(df_brightness.style.background_gradient(cmap='Greys_r', axis=None).format('{:.2f}'), height=1122)
+    with st.expander('夜空の明るさの推測値 @ 0.00:暗 - 99.0:明', expanded=True):
+      st.dataframe(df_brightness.style.background_gradient(cmap='Greys_r', axis=None).format('{:.2f}'), height=1122)
 
-        with st.expander('Moon phase. @月の輝面率 0.00：新月 - 1.00:満月'):
-            st.dataframe(df_phase.style.background_gradient(cmap='afmhot', axis=None).format('{:.2f}'), height=1122)
+    with st.expander('Moon phase. @月の輝面率 0.00：新月 - 1.00:満月'):
+      st.dataframe(df_phase.style.background_gradient(cmap='afmhot', axis=None).format('{:.2f}'), height=1122)
 
-        with st.expander('Alter the moon. @地平線からの月の高さ -90.0:真下 - 0:地平線 - 90.0:直上'):
-            st.dataframe(df_alt.style.background_gradient(cmap='bone', axis=None).format('{:.2f}'), height=1122)
+    with st.expander('Alter the moon. @地平線からの月の高さ -90.0:真下 - 0:地平線 - 90.0:直上'):
+      st.dataframe(df_alt.style.background_gradient(cmap='bone', axis=None).format('{:.2f}'), height=1122)
