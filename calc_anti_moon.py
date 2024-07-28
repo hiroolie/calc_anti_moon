234d97542c6b5b8a5d18f8ef0970b24092243c63
hiroolie <hiroolie@gmail.com>
2024-07-28 12:40:19 +0900

    Update calc_anti_moon.py
    move expander section.

--- calc_anti_moon.py
+++ calc_anti_moon.py
@@ -114,9 +114,9 @@ st.caption('月の輝面率と高さを掛けてみただけなので本当の�
 with st.container():
   # with st.expander('観測地点をクリック'):
     st_data = st_folium(m, width=1280, height=500)
-    if st_data["last_clicked"] is not None:
-      with st.expander('観測地点をクリック'):
-        st.write("Lat[緯度]:", st_data["last_clicked"].get('lat', 'not found'), "Lon[経度]:", st_data["last_clicked"].get('lng', 'not found'))
+    with st.expander('観測地点をクリック'):
+      if st_data["last_clicked"] is not None:
+      st.write("Lat[緯度]:", st_data["last_clicked"].get('lat', 'not found'), "Lon[経度]:", st_data["last_clicked"].get('lng', 'not found'))
 
 
 with st.container():
